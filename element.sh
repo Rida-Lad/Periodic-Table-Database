bc8edfb5c0d84552eba5aa7f050e3ed8fa119d41
RIDA LADIB <ridlad7@gmail.com>
2025-04-16 16:21:00 +0000

    chore: complete all database modifications and script

--- element.sh
+++ element.sh
@@ -26,7 +26,7 @@ if [[ -z $ELEMENT_INFO ]]; then
 fi    
 
 # Parse the result
-IFS='|' read -r ATOMIC_NUMBER NAME SYMBOL TYPE ATOMIC_MASS MELTING_POINT BOILING_POINT <<< "$ELEMENT_INFO"
+IFS='|' read -r ATOMIC_NUMBER NAME SYMBOL TYPE ATOMIC_MASS MELTING_POINT BOILING_POINT <<< "$ELEMENT_INFO"    
 
 # Output the information
 echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
